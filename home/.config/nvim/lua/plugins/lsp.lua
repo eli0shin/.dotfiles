cec37a6f1f128608d7646532e03ae9c912eac014
Eli Oshinsky <eli.oshinsky@betfanatics.com>
2026-04-29 14:16:03 -0400

    add more work packages

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -180,7 +180,7 @@ return { -- LSP Configuration & Plugins
       ts_ls = {
         cmd = { 'typescript-language-server', '--stdio' },
         root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
-        filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
+        filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
         single_file_support = true,
       },
       htmx = { enabled = false },
