d0d457a48cca49df9ab4bb32454a435305237215
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2026-08-11 08:53:07 -0400

    add herdr support to devbox connection

--- home/.config/fish/functions/devbox.fish
+++ home/.config/fish/functions/devbox.fish
@@ -1,3 +1,11 @@
 function devbox
-    mosh elioshinsky@devbox.home.arpa -- fish -ic t
+    argparse --exclusive h,t h t -- $argv
+    or return
+
+    set -l command herdr
+    if set -q _flag_t
+        set command t
+    end
+
+    mosh elioshinsky@devbox.home.arpa -- fish -ic $command
 end
